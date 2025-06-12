0ed522b05aa99cf27a1d6ebef54af2ca3e7017d0
Miguel Costa <mapcosta999@gmail.com>
2025-06-12 16:13:05 -0300

    adicionado o metodo particionar

--- ParticaoDeConjunto.java
+++ ParticaoDeConjunto.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class ParticaoDeConjunto {
   // PARA NUMEROS INTEIROS POSITIVOS
   private int[] conjunto;
-  private Boolean[][] memo;
+  private boolean[][] memo;
 
   public static void main(String[] args) {
     int[] conjunto = {1, 3, 4, 7, 5};
@@ -10,14 +14,16 @@ public class ParticaoDeConjunto {
     // int[] conjunto = {8, 1, 3, 1, 2};
     ParticaoDeConjunto pc = new ParticaoDeConjunto();
     
-    System.out.println(pc.podeParticionar(conjunto));
-    System.out.println(pc.podeParticionar_bottomUp(conjunto));
+    System.out.println(pc.podeParticionar_BottomUp(conjunto));
+    for (Integer[] arr: pc.particionar()) {
+      System.out.println(Arrays.toString(arr));
+    }
 
   }
 
-  private boolean podeParticionar_bottomUp(int[] conjunto) {
-    this.conjunto = conjunto;
+  private boolean podeParticionar_BottomUp(int[] conjunto) {
     int somaConjunto, metadeSoma, somaMenor;
+    this.conjunto = conjunto;
 
     somaConjunto = 0;
     for (int elem: conjunto) somaConjunto += elem;
@@ -26,13 +32,14 @@ public class ParticaoDeConjunto {
     if (somaConjunto % 2 != 0) return false;
 
     metadeSoma = somaConjunto / 2;
-    memo = new Boolean[conjunto.length + 1][metadeSoma + 1];
+    memo = new boolean[conjunto.length + 1][metadeSoma + 1];
 
     for (int i = 0; i < memo[0].length; i++) {
       memo[0][i] = false;
     }
     memo[0][0] = true;
-
+    
+    // memorização bottom-up 
     for (int i = 1; i < memo.length; i++) {
       for (int j = 0; j < memo[0].length; j++) {
         somaMenor = j - conjunto[i-1];
@@ -47,34 +54,47 @@ public class ParticaoDeConjunto {
     return memo[memo.length - 1][memo[0].length - 1];
   }
 
-  public boolean podeParticionar(int[] conjunto) {
-    this.conjunto = conjunto;
-    int somaConjunto, metadeSoma;
+  public Integer[][] particionar() {
+    HashSet<Integer> indicesSubconjunto;
+    ArrayList<Integer> subconjunto1, subconjunto2;
+    int soma, indice;
 
-    somaConjunto = 0;
-    for (int elem: conjunto) somaConjunto += elem;
-
-    if (somaConjunto % 2 != 0) return false;
-
-    metadeSoma = somaConjunto / 2;
-    memo = new Boolean[conjunto.length][metadeSoma + 1];
-
-    return podeParticionar(conjunto.length - 1, metadeSoma);
-  }
-
-  private boolean podeParticionar(int maiorIndice, int soma) {
-    boolean mantemSoma, diminuiSoma;
-
-    if (soma == 0) return true;
-    if (maiorIndice < 0 || soma < 0) return false;
-
-    if (memo[maiorIndice][soma] == null) {
-      mantemSoma = podeParticionar(maiorIndice - 1, soma);
-      diminuiSoma = podeParticionar(maiorIndice - 1, soma - conjunto[maiorIndice]);
-      memo[maiorIndice][soma] = mantemSoma || diminuiSoma;
+    if (memo == null || memo[memo.length - 1][memo[0].length - 1] == false) {
+      return null;
     }
 
-    return memo[maiorIndice][soma];
+    indicesSubconjunto = new HashSet<>();
+    subconjunto1 = new ArrayList<>();
+    subconjunto2 = new ArrayList<>();
+    indice = conjunto.length;
+    soma = 0;
+    for (int elem: conjunto) soma += elem;
+    soma /= 2;
+
+    while (soma > 0) {
+      if (memo[indice][soma] == true && memo[indice - 1][soma] == true) {
+        indice--;
+        continue;
+      }
+
+      indicesSubconjunto.add(indice - 1);
+      soma -= conjunto[indice - 1];
+      indice--;
+    }
+
+    for (int i = 0; i < conjunto.length; i++) {
+      if (indicesSubconjunto.contains(i)) {
+        subconjunto1.add(conjunto[i]);
+      } else {
+        subconjunto2.add(conjunto[i]);
+      }
+    }
+
+    Integer[][] s = {
+      subconjunto1.toArray(new Integer[0]),
+      subconjunto2.toArray(new Integer[0])
+    };
+    return s;
   }
 
 }
